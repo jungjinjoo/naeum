7aab5adf97e58904935799571e713669cf27821c
jungjinjoo <cilcil197911@gmail.com>
2025-05-26 14:18:14 +0900

    Add files via upload

---
+++ script/slide.js
@@ -0,0 +1,9 @@
+$(function () {
+    $(".nav>li").mouseenter(function () {
+        $(this).children(".sub").stop().slideDown();
+    })
+
+    $(".nav>li").mouseleave(function () {
+        $(this).children(".sub").stop().slideUp();
+    })
+});
