021c402e7d65295c4d8d798c832a008074ccb7d0
jungjinjoo <cilcil197911@gmail.com>
2025-05-26 16:40:44 +0900

    Add files via upload

--- script/slide.js
+++ script/slide.js
@@ -7,3 +7,76 @@ $(function () {
         $(this).children(".sub").stop().slideUp();
     })
 });
+
+// 내 js
+    AOS.init({
+      duration: 1500,
+      offset: 300,
+      easing: 'ease-out-back',
+    });
+
+// 메인배너부분
+  var swiper = new Swiper(".mySwiper", {
+      speed: 1000,
+      spaceBetween: 0,
+      pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+      },
+      navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+      },
+      autoplay: {
+        delay: 2500,
+        disableOnInteraction: false,
+      },
+
+
+    });
+
+// 뉴아이템부분
+    var swiper = new Swiper(".new", {
+      speed: 1000,
+      effect: "fade",
+      autoplay: {
+        delay: 2000,
+        disableOnInteraction: false,
+      },
+      mousewheel: true,
+      keyboard: true,
+      pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+      },
+    });
+
+// 이벤트부분
+  var swiper = new Swiper(".event", {
+      slidesPerView: 2,
+      spaceBetween: 10,
+      autoplay: {
+        delay: 2000,
+        disableOnInteraction: false,
+      },
+      pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+      },
+      loop: true,
+    });
+
+// 리뷰부분
+var swiper = new Swiper(".review", {
+      slidesPerView: 5,
+      spaceBetween: 10,
+      autoplay: {
+        delay: 2000,
+        disableOnInteraction: false,
+      },
+      pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+      },
+      loop: true,
+    });
